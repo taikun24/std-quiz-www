afc84b21b044510ad08c66c7621883ee1b7e8e42
taikun24 <133754274+taikun24@users.noreply.github.com>
2025-02-17 22:02:31 +0900

    I wanna fix it!

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ window.onload = async function () {
             setTimeout(to_main, 0);
         }
         if (custom_html) { loadCustomHTML(info); return; }
-        quiz = await load_quiz('../../' + info.url);
+        quiz = await load_quiz('../' + info.url);
         if (quiz == undefined || quiz.length == 0) onError('invaild_quiz');
         shuffle_array(quiz);
         filters.forEach(element => document.getElementById('filter-i').innerHTML += "<option value='" + element + "'>" + info.filter[element] + "</option>");
